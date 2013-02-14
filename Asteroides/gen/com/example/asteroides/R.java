066292b935f5ce4476016d28fcd7c6f034cdd22e
jesramgue@gmail.com <jesramgue@gmail.com@3028a14b-7d97-93f1-6899-284ea585c5f0>
2013-02-14 19:36:37 +0000

    Layout Tablet añadido.

--- Asteroides/gen/com/example/asteroides/R.java
+++ Asteroides/gen/com/example/asteroides/R.java
@@ -15,11 +15,12 @@ public final class R {
     }
     public static final class id {
         public static final int LinearLayout1=0x7f070000;
+        public static final int TableLayout1=0x7f070005;
         public static final int button1=0x7f070001;
         public static final int button2=0x7f070002;
         public static final int button3=0x7f070003;
         public static final int button4=0x7f070004;
-        public static final int menu_settings=0x7f070005;
+        public static final int menu_settings=0x7f070006;
     }
     public static final class layout {
         public static final int activity_asteroides=0x7f030000;
@@ -28,9 +29,13 @@ public final class R {
         public static final int activity_asteroides=0x7f060000;
     }
     public static final class string {
+        public static final int acerca_de=0x7f040005;
         public static final int app_name=0x7f040000;
+        public static final int configurar=0x7f040004;
         public static final int hello_world=0x7f040001;
+        public static final int jugar=0x7f040003;
         public static final int menu_settings=0x7f040002;
+        public static final int salir=0x7f040006;
     }
     public static final class style {
         /** 
@@ -53,9 +58,31 @@ public final class R {
         res/values-v11/styles.xml on API 14+ devices.
     
  API 14 theme customizations can go here. 
+
+        Base application theme, dependent on API level. This theme is replaced
+        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+    
+
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
+
+        Base application theme, dependent on API level. This theme is replaced
+        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+    
+
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
          */
         public static final int AppBaseTheme=0x7f050000;
         /**  Application theme. 
+ All customizations that are NOT specific to a particular API-level can go here. 
+ Application theme. 
+ All customizations that are NOT specific to a particular API-level can go here. 
+ Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f050001;
