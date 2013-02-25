9db9a9d747ee47e80e74b7b62746a171881952e8
jesramgue@gmail.com <jesramgue@gmail.com@3028a14b-7d97-93f1-6899-284ea585c5f0>
2013-02-25 17:03:09 +0000

    Hasta Icono hecho

--- Asteroides/gen/com/example/asteroides/R.java
+++ Asteroides/gen/com/example/asteroides/R.java
@@ -14,19 +14,24 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f070000;
-        public static final int TableLayout1=0x7f070005;
-        public static final int button1=0x7f070001;
-        public static final int button2=0x7f070002;
-        public static final int button3=0x7f070003;
-        public static final int button4=0x7f070004;
-        public static final int menu_settings=0x7f070006;
+        public static final int LinearLayout1=0x7f070001;
+        public static final int TableLayout1=0x7f070006;
+        public static final int TextView01=0x7f070000;
+        public static final int acercaDe=0x7f070009;
+        public static final int button1=0x7f070002;
+        public static final int button2=0x7f070003;
+        public static final int button3=0x7f070004;
+        public static final int button4=0x7f070005;
+        public static final int config=0x7f070008;
+        public static final int menu_settings=0x7f070007;
     }
     public static final class layout {
-        public static final int activity_asteroides=0x7f030000;
+        public static final int acercade=0x7f030000;
+        public static final int activity_asteroides=0x7f030001;
     }
     public static final class menu {
         public static final int activity_asteroides=0x7f060000;
+        public static final int menu=0x7f060001;
     }
     public static final class string {
         public static final int acerca_de=0x7f040005;
--- Asteroides/src/com/example/asteroides/Asteroides.java
+++ Asteroides/src/com/example/asteroides/Asteroides.java
@@ -1,22 +1,67 @@
 package com.example.asteroides;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class Asteroides extends Activity {
+	private Button bSalir;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_asteroides);
+		bSalir = (Button) findViewById(R.id.button4);
+		// 1ª Listener del boton salir en Código (actualmente está siendo usado
+		// por XML)
+		/*
+		 * bSalir.setOnClickListener(new OnClickListener() { public void
+		 * onClick(View view) { salir(view);
+		 * 
+		 * }
+		 * 
+		 * });
+		 */
+	}
+
+	/*
+	 * @Override public boolean onCreateOptionsMenu(Menu menu) { // Inflate the
+	 * menu; this adds items to the action bar if it is present.
+	 * getMenuInflater().inflate(R.menu.activity_asteroides, menu); return true;
+	 * }
+	 */
+
+	public void lanzarAcercaDe(View view) {
+		Intent i = new Intent(this, AcercaDe.class);
+		startActivity(i);
+	}
+
+	public void salir(View view) {
+		finish();
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_asteroides, menu);
+		super.onCreateOptionsMenu(menu);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu, menu);
 		return true;
+		/** true -> el menú ya está visible */
 	}
 
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.acercaDe:
+			lanzarAcercaDe(null);
+			break;
+		}
+		return true;
+		/** true -> consumimos el item, no se propaga */
+	}
 }
